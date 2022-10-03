8636e73b009b5a792a000adf96d00b1be470905c
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-10-03 18:59:38 +0545

    create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    phone = models.PositiveIntegerField(max_length=10)
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=500)
+    terms_and_condition = models.Char
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
