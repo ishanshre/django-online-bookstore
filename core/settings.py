2d6556e6c59b663b529ff1b6a2f61def2c789fec
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-10-14 10:23:27 +0545

    refactor: change STATIC_ROOT value, remove all directory inside static and run collectstatic

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILE_DIR = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
